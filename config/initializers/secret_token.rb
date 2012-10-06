71c1c9a9636bed55731d7158dd2402643f6a93bb
Lucas Vidal <lucas.vidal@aycron.com>
2012-10-06 14:18:42 -0300

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Farmagestion::Application.config.secret_token = 'fad1546c4a942cfa4e761b0362b93191615c72c1c1f42d877120f3f8e7a39980ff1e461fa57f414b444a3919c1c159c7bb8c16ad1ca16c6877b8bf155db6d733'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Farmagestion::Application.config.session_store :cookie_store, key: '_farmagestion_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Farmagestion::Application.config.session_store :active_record_store
